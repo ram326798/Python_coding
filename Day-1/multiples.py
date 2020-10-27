f90bd5c0744ca85a4cdf814dd9f1cebb77aa5e73
ram326798 <41385893+ram326798@users.noreply.github.com>
2020-10-27 22:58:18 +0530

    Update multiples.py
    Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.

--- Day-1/multiples.py
+++ Day-1/multiples.py
@@ -1,7 +1,7 @@
 def multiples():
     for i in range(100):
         if (i%3 ==0 and i%5==0):
-            print("FizzBuzz")
+            print("Fizz Buzz")
         elif i%3 ==0:
             print("Fizz")
         elif i%5==0:
