6cc5ec73ec58d2c2e4e9521d24defe5b0f195103
ram326798 <41385893+ram326798@users.noreply.github.com>
2020-11-02 21:09:47 +0530

    Update sys-module.py

--- Day-2/sys-module.py
+++ Day-2/sys-module.py
@@ -2,3 +2,10 @@ import sys
 
 print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
+
+
+# # output:
+# python sys-module.py ram krish chimmani
+
+# Number of arguments: 4 arguments.
+# Argument List: ['sys-module.py', 'ram', 'krish', 'chimmani']
